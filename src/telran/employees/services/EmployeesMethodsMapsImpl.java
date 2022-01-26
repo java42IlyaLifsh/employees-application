496fa45f3f5ee4e684c3e4b0bfad410502aaffac
Yuragranovsky <yuragranovsky@gmail.com>
2022-01-26 21:16:08 +0200

    cw #34 initial code with TODO's for hw #34

---
+++ src/telran/employees/services/EmployeesMethodsMapsImpl.java
@@ -0,0 +1,95 @@
+package telran.employees.services;
+
+import telran.employees.dto.Employee;
+import telran.employees.dto.ReturnCode;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+public class EmployeesMethodsMapsImpl implements EmployeesMethods {
+ private HashMap<Long, Employee> mapEmployees = new HashMap<>(); //key employee's id, value - employee
+ private TreeMap<Integer, List<Employee>> employeesAge= new TreeMap<>(); //key - age, value - list of employees with the same age
+ private TreeMap<Integer, List<Employee>> employeesSalary = new TreeMap<>(); //key - salary,
+ //value - list of employees with the same salary
+ private HashMap<String, List<Employee>> employeesDepartment = new HashMap<>();
+	@Override
+	public ReturnCode addEmployee(Employee empl) {
+		if (mapEmployees.containsKey(empl.id)) {
+			return ReturnCode.EMPLOYEE_ALREADY_EXISTS;
+		}
+		Employee emplS = new Employee(empl.id, empl.name, empl.birthDate, empl.salary, empl.department);
+		mapEmployees.put(emplS.id, emplS);
+		employeesAge.computeIfAbsent(getAge(emplS), k -> new LinkedList<Employee>()).add(emplS);
+		employeesSalary.computeIfAbsent(emplS.salary, k -> new LinkedList<Employee>()).add(emplS);
+		employeesDepartment.computeIfAbsent(emplS.department, k -> new LinkedList<Employee>()).add(emplS);
+		
+		return ReturnCode.OK;
+	}
+
+	private Integer getAge(Employee emplS) {
+		
+		return (int)ChronoUnit.YEARS.between(emplS.birthDate, LocalDate.now());
+	}
+
+	@Override
+	public ReturnCode removeEmployee(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Employee> getAllEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee getEmployee(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Employee> getEmployeesByAge(int ageFrom, int ageTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Employee> getEmployeesBySalary(int salaryFrom, int salaryTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Employee> getEmployeesByDepartment(String department) {
+		List<Employee> employees = employeesDepartment.getOrDefault(department, Collections.emptyList());
+		
+		return employees.isEmpty() ? employees : getEmployeesFromList(employees);
+	}
+
+	private Iterable<Employee> getEmployeesFromList(List<Employee> employees) {
+		
+		return employees.stream().map(e ->
+		new Employee(e.id, e.name, e.birthDate, e.salary, e.department)).toList();
+	}
+
+	@Override
+	public Iterable<Employee> getEmployeesByDepartmentAndSalary(String department, int salaryFrom, int salaryTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnCode updateSalary(long id, int newSalary) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnCode updateDepartment(long id, String newDepartment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/telran/employees/services/EmployeesMethodsTest.java
@@ -0,0 +1,99 @@
+package telran.employees.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import telran.employees.dto.*;
+import java.util.*;
+class EmployeesMethodsTest {
+private static final long ID1 = 123;
+private static final String NAME = "name";
+private static final LocalDate BIRTHDATE1 = LocalDate.of(2000, 1, 1);
+private static final int SALARY1 = 5000;
+private static final String DEPARTMENT1 = "department1";
+private static final long ID2 = 124;
+private static final long ID3 = 125;
+private static final long ID4 = 126;
+private static final long ID5 = 127;
+private static final long ID6 = 128;
+private static final LocalDate BIRTHDATE2 = LocalDate.of(1995, 1, 1);;
+private static final LocalDate BIRTHDATE3 = LocalDate.of(1997, 1, 1);;
+private static final LocalDate BIRTHDATE4 = LocalDate.of(1970, 1, 1);;
+private static final LocalDate BIRTHDATE5 = LocalDate.of(1971, 1, 1);;
+private static final LocalDate BIRTHDATE6 = LocalDate.of(1980, 1, 1);
+private static final String DEPARTMENT2 = "department2";
+private static final int SALARY2 = 6000;
+private static final int SALARY3 = 1000;;
+EmployeesMethods employees; 
+Employee empl1 = new Employee(ID1, NAME, BIRTHDATE1, SALARY1, DEPARTMENT1);
+Employee empl2 = new Employee(ID2, NAME, BIRTHDATE2, SALARY1, DEPARTMENT1);
+Employee empl3 = new Employee(ID3, NAME, BIRTHDATE3, SALARY2, DEPARTMENT1);
+Employee empl4 = new Employee(ID4, NAME, BIRTHDATE4, SALARY2, DEPARTMENT2);
+Employee empl5 = new Employee(ID5, NAME, BIRTHDATE5, SALARY3, DEPARTMENT2);
+Employee empl6 = new Employee(ID6, NAME, BIRTHDATE6, SALARY3, DEPARTMENT2);
+List<Employee> employeesList = Arrays.asList(empl1,empl2,empl3,empl4,empl5,empl6);
+	@BeforeEach
+	void setUp() throws Exception {
+		employees = new EmployeesMethodsMapsImpl();
+		employeesList.forEach(employees::addEmployee);
+		
+	}
+
+	@Test
+	void testAddEmployee() {
+		assertEquals(ReturnCode.EMPLOYEE_ALREADY_EXISTS, employees.addEmployee(empl1));
+		assertEquals(ReturnCode.OK, 
+				employees.addEmployee(new Employee(ID1 + 10000, NAME, BIRTHDATE1, SALARY1, DEPARTMENT1)));
+	}
+
+	@Test
+	void testRemoveEmployee() {
+		//TODO
+	}
+
+	@Test
+	void testGetAllEmployees() {
+		//TODO
+	}
+
+	@Test
+	void testGetEmployee() {
+		//TODO
+	}
+
+	@Test
+	void testGetEmployeesByAge() {
+		//TODO
+	}
+
+	@Test
+	void testGetEmployeesBySalary() {
+		//TODO
+	}
+
+	@Test
+	void testGetEmployeesByDepartment() {
+		List<Employee> expected = Arrays.asList(empl1, empl2, empl3);
+		assertIterableEquals(expected, employees.getEmployeesByDepartment(DEPARTMENT1));
+	}
+
+	@Test
+	void testGetEmployeesByDepartmentAndSalary() {
+		//TODO
+	}
+
+	@Test
+	void testUpdateSalary() {
+		//TODO
+	}
+
+	@Test
+	void testUpdateDepartment() {
+		//TODO
+	}
+
+}
